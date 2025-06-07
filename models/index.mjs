68ad86a19150936b51070eacb64a27a124bd06c4
renatosanz <renato.sanchez3103@gmail.com>
2025-06-07 12:12:34 -0600

    add post-like-user relation

--- models/index.mjs
+++ models/index.mjs
@@ -1,8 +1,11 @@
+import { Like } from "./Like.mjs";
 import { Receta } from "./Recetas.mjs";
 import { User } from "./User.mjs";
 
 // Definir relaciones
 Receta.belongsTo(User, { foreignKey: "user_id" });
 User.hasMany(Receta, { foreignKey: "user_id" });
+Like.belongsTo(Receta, { foreignKey: "post_id" });
+User.hasMany(Like, { foreignKey: "user_id" });
 
 export { Receta, User };
